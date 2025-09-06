ca097a614a5f190b4e6c20d1b27f8992f3e30c84
lanaal <svetlana.alyabjeva@gmail.com>
2025-09-06 15:28:24 +0300

    start project

--- src/main/java/dto/RegistrationBodyDto.java
+++ src/main/java/dto/RegistrationBodyDto.java
@@ -1,4 +1,18 @@
 package dto;
 
-public class RegistrationBoduDto {
+import lombok.*;
+
+@Setter
+@Getter
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class RegistrationBodyDto {
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+
 }
--- src/main/java/interfaces/BaseApi.java
+++ src/main/java/interfaces/BaseApi.java
@@ -1,4 +1,6 @@
 package interfaces;
 
 public interface BaseApi {
+    String BASE_URL = "https://ilcarro-backend.herokuapp.com";
+    String REGISTRATION_URL = "/v1/user/registration/usernamepassword";
 }
